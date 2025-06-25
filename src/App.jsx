7f618583d714724744034dfd48ba09d463ad26e5
Aditya <ak.adityakumar421@gmail.com>
2025-06-25 23:09:51 +0530

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import {useEffect} from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+// import "prismjs/components/prism-jsx"
+import prism from "prismjs"
+import './App.css'
+import { useState } from 'react'
+import Editor from 'react-simple-code-editor'
+import axios from 'axios'
+import Markdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
+import 'highlight.js/styles/github-dark.css'
+
+function App() {
+  const [code, setCode] = useState(``);
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  const [review, setReview] = useState(``);
+  async function reviewCode() {
+    axios.post('https://code-critic-ai-rho.vercel.app/ai/get-response', { prompt: code })
+      .then(response => {
+        // console.log(response.data);
+        setReview(response.data);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              placeholder='Paste your code here...'
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 15,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }}
+            />
+          </div>
+          <div className="review" onClick={reviewCode}>
+            Review
+          </div>    
+        </div>
+        <div className="right">
+          <Markdown
+            rehypePlugins={[rehypeHighlight]}
+          >{review}</Markdown>
+        </div>
+      </main>
+    </>
+  )
+}
+export default App
